0dd58c942429f0008398d37750474455e7adc81b
agakaczmarska <106772237+agakaczmarska@users.noreply.github.com>
2023-11-23 16:25:10 +0100

    dodaj

--- csv.py
+++ csv.py
@@ -1,3 +1,12 @@
 with open('C:\\Users\\akaczmarska\\Desktop\\Moj_folder\\rozliczenie.csv', 'r') as plik1: # read czyta caly plik
-    content = plik1.read()
-    print(content)
+  # #  content = plik1.read(5)
+  #   print(content)
+  #   content = plik1.read(5)
+  #   # metoda read wrzuca wszystko do jednego stringa, metoda readlines wyswietla liste stringow, readlines przeczyta wszystko
+  #   content = plik1.read(5)  # przeczyta piec znakow; pierwszych badz kolejnych
+  #   print(content)
+  #   for line in range(10):
+  #       content = plik1.readline()
+  #       print(content)
+    content = plik1.readlines()
+print(content)
