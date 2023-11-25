8055ec897166d395f5f8363f45719b0f1670d069
agakaczmarska <106772237+agakaczmarska@users.noreply.github.com>
2023-11-25 09:22:28 +0100

    dodane

--- csv.py
+++ csv.py
@@ -10,3 +10,23 @@ with open('C:\\Users\\akaczmarska\\Desktop\\Moj_folder\\rozliczenie.csv', 'r') a
   #       print(content)
     content = plik1.readlines()
 print(content)
+
+# przygotowanie danych
+for i in range(len(content)):
+  content[i] = content[i].split(',')
+print(content)
+
+# #obliczanie sredniej wyplaty
+# total = 0
+# for line in content[1:]:
+#   total += int(line[1])
+# print(f'srednia {round(total/len(content)-1,2)}')
+
+#obliczanie ilosci kobiet na macierzynskim
+kobiety_na_macierzyskim = 0
+for line in content[1:]:
+  if line[4][0].lower() == 't' and line[3].lower() == 'k':
+    kobiety_na_macierzyskim += 1
+print(f'Liczba kobiet na macierzynskim wynosi{kobiety_na_macierzyskim}')
+#if line[4][0].lower() == 't' and line[3].lower() == 'k' --mozna napisac \n czyli znak nowej lini
+    #lib mozemy to obejsc line.[4][0]
---
+++ objektowka.py
@@ -0,0 +1,20 @@
+Asia = int(100)
+Basia = int(100)
+Pawel = int(100)
+
+AsiaToPawel = 20
+Asia = Asia - AsiaToPawel
+Pawel = Pawel + AsiaToPawel
+BasiaToPawel = 50
+Basia = Basia - BasiaToPawel
+Pawel = Pawel + BasiaToPawel
+PawelToAsia = 60
+Pawel = Pawel - PawelToAsia
+Asia = Asia + PawelToAsia
+BasiaToAsia = 30
+Basia = Basia - BasiaToAsia
+Asia = Asia + BasiaToAsia
+
+print(Asia)
+print(Basia)
+print(Pawel)
